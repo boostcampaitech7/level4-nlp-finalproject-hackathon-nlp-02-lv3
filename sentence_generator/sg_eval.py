193354b99e48981f02587ee1411217c63bc89969
Sujinkim-625 <hidong1015@dgu.ac.kr>
2025-02-02 19:56:04 +0900

    refactor: completion_executor 모듈 적용

--- sentence_generator/sg_eval.py
+++ sentence_generator/sg_eval.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import json
 import re
 
 from loguru import logger
-import requests
+from modules import CompletionExecutor
 import yaml
 
 
@@ -18,46 +17,6 @@ config = load_config("../config/config_sg_eval.yaml")
 API_KEY = config["API"]["API_KEY"]
 REQUEST_ID = config["API"]["REQUEST_ID"]
 COMPLETION_HOST_URL = config["API"]["HOST_URL"]
-
-
-class CompletionExecutor:
-    def __init__(self, host, api_key, request_id):
-        self._host = host
-        self._api_key = api_key
-        self._request_id = request_id
-
-    def execute(self, completion_request):
-        headers = {
-            "Authorization": self._api_key,
-            "X-NCP-CLOVASTUDIO-REQUEST-ID": self._request_id,
-            "Content-Type": "application/json; charset=utf-8",
-            "Accept": "text/event-stream",
-        }
-
-        final_content = ""
-        try:
-            with requests.post(
-                self._host + "/testapp/v1/chat-completions/HCX-003",
-                headers=headers,
-                json=completion_request,
-                stream=True,
-            ) as r:
-                r.raise_for_status()
-                for line in r.iter_lines():
-                    if line:
-                        decoded_line = line.decode("utf-8")
-                        # print("Received Line:", decoded_line)  # 디버깅용 출력
-                        if decoded_line.startswith("data:"):
-                            try:
-                                data = json.loads(decoded_line[5:])
-                                if "message" in data and "content" in data["message"]:
-                                    final_content = data["message"]["content"]
-                            except json.JSONDecodeError:
-                                continue
-        except requests.RequestException as e:
-            logger.error(f"Request failed: {e}")
-            return None
-        return final_content
 
 
 def process_response(content_result):
