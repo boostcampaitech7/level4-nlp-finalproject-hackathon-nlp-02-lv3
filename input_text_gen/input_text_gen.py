78dd7dc5c20f1d808d2825d50caa3ad32083b520
koreannn <ghdtjdwo5@gmail.com>
2025-02-03 06:23:15 +0000

    chore: Add config_input_text_gen.yaml

--- input_text_gen/input_text_gen.py
+++ input_text_gen/input_text_gen.py
@@ -13,13 +13,14 @@ def load_config(file_path):
     return config
 
 
-config = load_config("../config.yaml")
+config_api = load_config("../config/config_api.yaml")
+config_input_text_gen = load_config("../config/config_input_text_gen.yaml")
 
-API_KEY = config["API"]["API_KEY"]
-REQUEST_ID = config["API"]["REQUEST_ID"]
-COMPLETION_HOST_URL = config["API"]["HOST_URL"]
+API_KEY = config_api["API"]["API_KEY"]
+REQUEST_ID = config_api["API"]["REQUEST_ID"]
+COMPLETION_HOST_URL = config_api["API"]["HOST_URL"]
 
-fiction_df = pd.read_csv(config["Origin_fiction_dir"])
+fiction_df = pd.read_csv(config_input_text_gen["Origin_fiction_dir"])
 fiction_content = fiction_df["tcontent"]
 
 
@@ -65,28 +66,27 @@ if __name__ == "__main__":
     )
     content_list = []
 
-    # for fiction in fiction_content:
-    for i in range(3):
+    for i in range(len(fiction_content)):
         logger.info(f"{i+1}/{len(fiction_content)} Input_text 생성 중..")
         preset_text = [
             {
-                "role": config["Input_text_gen_LLM"]["preset_text"]["system"]["role"],
+                "role": config_input_text_gen["Input_text_gen_LLM"]["preset_text"]["system"]["role"],
                 # content : content(KR), content(US) > 한국어 프롬프트로 전달, 영어 프롬프트로 전달
-                "content": config["Input_text_gen_LLM"]["preset_text"]["system"]["content_KR"],
+                "content": config_input_text_gen["Input_text_gen_LLM"]["preset_text"]["system"]["content_US"],
             },
-            {"role": config["Input_text_gen_LLM"]["preset_text"]["user"]["role"], "content": f"{fiction_content[i]}"},
+            {"role": config_input_text_gen["Input_text_gen_LLM"]["preset_text"]["user"]["role"], "content": f"{fiction_content[i]}"},
         ]
 
         request_data = {
             "messages": preset_text,
-            "topP": config["Input_text_gen_LLM"]["request_params"]["topP"],
-            "topK": config["Input_text_gen_LLM"]["request_params"]["topK"],
-            "maxTokens": config["Input_text_gen_LLM"]["request_params"]["maxTokens"],
-            "temperature": config["Input_text_gen_LLM"]["request_params"]["temperature"],
-            "repeatPenalty": config["Input_text_gen_LLM"]["request_params"]["repeatPenalty"],
-            "stopBefore": config["Input_text_gen_LLM"]["request_params"]["stopBefore"],
-            "includeAiFilters": config["Input_text_gen_LLM"]["request_params"]["includeAiFilters"],
-            "seed": config["Input_text_gen_LLM"]["request_params"]["seed"],
+            "topP": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["topP"],
+            "topK": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["topK"],
+            "maxTokens": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["maxTokens"],
+            "temperature": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["temperature"],
+            "repeatPenalty": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["repeatPenalty"],
+            "stopBefore": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["stopBefore"],
+            "includeAiFilters": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["includeAiFilters"],
+            "seed": config_input_text_gen["Input_text_gen_LLM"]["request_params"]["seed"],
         }
 
         try:
@@ -95,8 +95,9 @@ if __name__ == "__main__":
                 logger.info(f"텍스트 생성 성공:\n {generated_content[:50]}...\n")
             else:
                 logger.warning(f"[{i+1}] 생성된 텍스트가 비어있음\n")
+                logger.warning(f"[{i+1}] {generated_content[:50]}...\n")
             content_list.append(generated_content)
-        # content_list.append(completion_executor.execute(request_data))
+            
         except Exception as e:
             logger.error(f"[{i+1}] 텍스트 생성 중 오류 발생: {str(e)}\n")
             content_list.append(None)
@@ -104,4 +105,4 @@ if __name__ == "__main__":
     logger.info("모든 Input_text 생성 완료.")
     fiction_prompt_df = pd.DataFrame({"musicgen_input_text": content_list})
     new_fiction_df = pd.concat([fiction_df, fiction_prompt_df], axis=1)
-    new_fiction_df.to_csv(config["New_fiction_dir"], index=False)
+    new_fiction_df.to_csv(config_input_text_gen["New_fiction_dir"], index=False)
