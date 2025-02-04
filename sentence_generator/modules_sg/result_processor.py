7f7b07f066ac07017141cd2299eddbcfd92bfbde
Sujinkim-625 <hidong1015@dgu.ac.kr>
2025-02-04 15:38:02 +0900

    feat: sentence_generate pipeline code

--- sentence_generator/modules_sg/result_processor.py
+++ sentence_generator/modules_sg/result_processor.py
@@ -67,6 +67,7 @@ def process_result(result):
 
         logger.info(f"Probability Score: {proba_score}")
         logger.info(f"Total Probability Score: {total_proba_score}")
+        return result["total_score"], total_proba_score
 
     else:
         logger.error("Failed to retrieve valid content from response.")
