be4c1522b16b7d8868a8e8f11b3c64f603ba98e5
Sujinkim-625 <hidong1015@dgu.ac.kr>
2025-02-04 20:08:52 +0900

    feat: threshold 점수를 넘는 문장 n개 생성

--- sentence_generator/sg_pipeline.py
+++ sentence_generator/sg_pipeline.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import time
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+from loguru import logger
 from sentence_generator.modules_sg.sentence_generate import run_sg
 from sentence_generator.modules_sg.sentence_generate_eval import run_sg_eval
 
 
+def generate_n_sentences(n, threshold_proba_score, threshold_correctness):
+    """
+    문장을 생성하고 평가하여 일정 점수 이상일 경우 리스트에 추가하는 함수.
+    """
+    generated_texts = []
+    while len(generated_texts) < n:
+        original_text, generated_text = run_sg()
+        if not generated_text:
+            time.sleep(5)
+            continue
+
+        sum_scores, proba_score, sum_proba_scores = run_sg_eval(original_text, generated_text)
+        if (
+            sum_proba_scores is not None
+            and sum_proba_scores >= threshold_proba_score
+            and proba_score[0] >= threshold_correctness
+        ):
+            generated_texts.append(generated_text.replace("\n", " "))
+        else:
+            logger.warning("Score below threshold. Retrying...")
+            time.sleep(5)
+
+    return generated_texts
+
+
 if __name__ == "__main__":
-    original_text, generated_text = run_sg()
-    sum_scores, proba_score, sum_proba_scores = run_sg_eval(original_text, generated_text)
-    print(sum_scores)
-    print(proba_score)
-    print(sum_proba_scores)
+    generated_texts = generate_n_sentences(1, 30, 7)
+    print(generated_texts)
